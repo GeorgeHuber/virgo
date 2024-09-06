83fc5b975b17bd137a48da70e4c7750c82eaa956
Quan <qq1990@g.rit.edu>
2024-09-06 19:44:30 -0400

    fix error on Linux server by correcting scrollable frame

--- virgo/ui/components/scrollable_frame.py
+++ virgo/ui/components/scrollable_frame.py
@@ -10,27 +10,29 @@ class ScrollableFrame(tk.Frame):
         self.grid_columnconfigure(0, weight=1)
         self.canvas = tk.Canvas(self)
         self.hackyRefreshRect = None
-        self.scrollbar = tk.Scrollbar(self, orient=tk.HORIZONTAL, command = self.canvas.yview)
+        self.scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL, command=self.canvas.yview)
         self.canvas.grid(row=0, column=0, sticky="nsew")
         self.scrollbar.grid(row=0, column=1, sticky="nsew")
-        self.canvas.configure(yscrollcommand=self.scrollbar.set)
-        self.bind(
-            '<Configure>', self.on_configure
-        )
         self.frame = tk.Frame(self.canvas)
-        os = sys.platform
-        if os == "darwin":
-            self.offset = 1.2
-        else:
-            self.offset = 120
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.canvas.create_window(0, 0, anchor="nw", window=self.frame)
+        self.frame.bind("<Configure>", self.on_configure)
+        self.canvas.bind_all("<MouseWheel>", self.on_mousewheel)
+        os_type = sys.platform
+        self.offset = 1.2 if os_type == "darwin" else 120
+
     def on_mousewheel(self, e):
-        #TODO: make this work on other OS
-        self.canvas.yview_scroll(int(-1*(e.delta)//self.offset), "units")
+        # Normalize mouse wheel event delta for cross-platform compatibility
+        delta = -1 * (e.delta // self.offset)
+        self.canvas.yview_scroll(int(delta), "units")
         self.canvas.update_idletasks()
+
     def on_configure(self, e):
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
     def get(self):
         return self.frame
+    
     def build(self, master_scroll=None):
         self.id = self.canvas.create_window(0, 0, anchor="nw", window=self.frame, tags="scroll")
         utils.bind_all_recur(self)
@@ -38,8 +40,8 @@ class ScrollableFrame(tk.Frame):
             self.canvas.bind_class(self.id, "<MouseWheel>", master_scroll)
         else:
             self.canvas.bind_class(self.id, "<MouseWheel>", self.on_mousewheel)
+
     def hackyRefresh(self):
         if self.hackyRefreshRect:
             self.canvas.delete(self.hackyRefreshRect)
         self.hackyRefreshRect =  self.canvas.create_rectangle(0,0,10,10,fill="blue")
-        
